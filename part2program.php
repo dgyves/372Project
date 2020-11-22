f4b02481e9b23cdd68eacd1f5b2535ad53971c88
dgyves <diegyves2@gmail.com>
2020-11-21 17:49:41 -0700

    "added test file"

--- part2program.php
+++ part2program.php
@@ -1,12 +1,60 @@
-<!DOCTYPE html>
-<html>
-<body>
-
 <?php
-// First commit
+
+function parseFile(array $arg){
+	// Read command line for filename
+	if (sizeof($arg) != 2){
+		echo "Please only include a file name.\n";
+		die;
+	}
+
+	$i = 0;
+	$data = array();
+
+	$myfile = fopen("test.txt", "r") or die("Unable to open file!");
+	// Output one line until end-of-file
+	while(!feof($myfile)) {
+		$str = fgets($myfile);
+		
+		if ($i == 0){
+			$k = intval($str);
+			$data['k'] = $k;
+		} else if ($i == 1){
+			$n = intval($str);
+			$data['n'] = $n;
+		} else {
+			//$data = explode(" ",$str);
+			$data[$i-2] = trim($str);
+			//$x = $data[0] ;
+			//$y = $data[1];
+			///echo "x: " . $x . " ";
+			//echo "y: " . $y . "\n";
+		}
+		$i++;
+	}
+	fclose($myfile);
+
+	if($k > $n){
+		die("Error: 'k' cannot be greater than 'n'.\n");
+	}
+
+	return $data;
+}
+
+function kClustering($data){
+	$k = $data['k'];
+	$n = $data['n'];
+	unset($data['k']);
+	unset($data['n']);
+	echo "k: " . $k . "\n";
+	echo "n: " . $n . "\n";
+	var_dump($data);
+
+	$count = 0;
+	
+}
 
 
+
+$data = parseFile($argv);
+kClustering($data);
 ?>
-
-</body>
-</html>
